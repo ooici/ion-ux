f214c6407f9e50ababb351a119d1901cfbb7ba0e
Alex Clemesha <clemesha@ucsd.edu>
2011-11-17 13:20:31 -0800

    Renamed second 'index' to 'dashboard' to avoid collision.

--- main.py
+++ main.py
@@ -7,11 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-
 @app.route('/dashboard')
-def index():
+def dashboard():
     return render_template("dashboard.html")
-
 
 @app.route('/profile/<name>', methods=["GET", "POST"])
 def userprofile(name):
