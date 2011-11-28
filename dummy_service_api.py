892ccb6da8f5b38c1556670d3dd397ae491466c8
Alex Clemesha <clemesha@ucsd.edu>
2011-11-28 15:28:07 -0800

    Put in the remaining /dataResource methods.

--- dummy_service_api.py
+++ dummy_service_api.py
@@ -4,8 +4,16 @@ class ServiceApi(object):
 
     @staticmethod
     def data_resource(request_args):
-        dummy_data.DATA.update({"dataResourceSummary":dummy_data.ALL_RESOURCES_DATA})
-        return dummy_data.DATA
+        action = request_args.get("action")
+        if action == "detail":
+            dummy_data.DATA["source"]["visualization_url"] = "http://visualize.whirledpeas.edu/crazy-visuals"
+            dummy_data.DATA.update({"dataResourceSummary":dummy_data.DATARESOURCESUMMARY_ONE})
+            return dummy_data.DATA
+        elif action == "findByUser":
+            return dummy_data.FINDBYUSER
+        else:
+            dummy_data.DATA.update({"dataResourceSummary":dummy_data.ALL_RESOURCES_DATA})
+            return dummy_data.DATA
 
     @staticmethod
     def subscription(request_args):
