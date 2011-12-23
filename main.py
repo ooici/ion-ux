867dfa0a7b959446638b739f86ee15f48046f668
Todd Boteler <tboteler@gmail.com>
2011-12-23 10:51:19 -0800

    Various loose ends, RESTful calls to service gateway.

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from flask import Flask, request, jsonify, render_template
+# from restclient import GET
+import requests
 
 app = Flask(__name__)
 
+SERVICE_GATEWAY_HOST = 'http://localhost:5000/test/bank/what'
 
 PRODUCTION = False #more configurable in the future.
 if PRODUCTION:
     from service_api import ServiceApi
 else:
     from dummy_service_api import ServiceApi
-
 
 @app.route('/')
 def index():
@@ -28,17 +30,27 @@ def subscription():
     resp_data = ServiceApi.subscription(request.args)
     return jsonify(resp_data)
 
+@app.route('/service-bridge')
+def connect_to_service():
+    # test with request contexts later
+    url = "http://localhost:5000/ion-service/rest/client"
+    
+    
+
 # - - - - - - - -
 
 @app.route('/new', methods=["GET"])
 def new_template():
     return render_template('ux-dashboard.html')
 
-@app.route('/services/<service_name>/<service_action>')
-def service_router(service_name, service_action):
-    userid = "returned from cookie."
-    repl = ServiceApi.parse_request(service_name=service_name, service_action=service_action)
-    return repl
+@app.route('/fake-service')
+def fake_service(methods=["GET"]):
+    resp_data = requests.get('http://localhost:5000/resource/instrument_model/list')
+    return resp_data.content
 
+@app.route('/dynamic-table')
+def dynamic_table(methods=["GET"]):
+    return render_template('ux-dashboard-demo.html')
+    
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=3000)
