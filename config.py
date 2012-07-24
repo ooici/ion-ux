dca30260d9d1a0122e57c474d8a082fbf3d7b26a
Todd Boteler <tboteler@gmail.com>
2012-07-23 17:08:57 -0700

    Added CACHED_LAYOUT to config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# flask server machine name
+FLASK_HOST = 'localhost'
+FLASK_PORT = 3000 # flask server port number
+
+# Service gateway machine name
+GATEWAY_HOST = 'localhost'
+GATEWAY_PORT=5000 # Service gateway port number
+
+LOGGED_IN = True
+PRODUCTION = False
+CACHED_LAYOUT = True
+
+# Be sure to define a secret key to enable session cookies
+SECRET_KEY = 'foo'
