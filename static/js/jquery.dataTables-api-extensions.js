70886064777b943cf62acd369eee71226755edce
Alex Clemesha <clemesha@ucsd.edu>
2012-08-29 18:13:03 -0700

    Datatable widget refactor complete, now works as a Backbone view.

--- static/js/jquery.dataTables-api-extensions.js
+++ static/js/jquery.dataTables-api-extensions.js
@@ -1,26 +1,29 @@
-$.fn.dataTableExt.oApi.fnFilterClear  = function ( oSettings )
-{
+$.fn.dataTableExt.oApi.fnFilterClear  = function(oSettings){
     /* Remove global filter */
     oSettings.oPreviousSearch.sSearch = "";
-      
     /* Remove the text of the global filter in the input boxes */
-    if ( typeof oSettings.aanFeatures.f != 'undefined' )
-    {
+    if ( typeof oSettings.aanFeatures.f != 'undefined' ){
         var n = oSettings.aanFeatures.f;
         for ( var i=0, iLen=n.length ; i<iLen ; i++ )
         {
             $('input', n[i]).val( '' );
         }
     }
-      
     /* Remove the search text for the column filters - NOTE - if you have input boxes for these
-     * filters, these will need to be reset
-     */
-    for ( var i=0, iLen=oSettings.aoPreSearchCols.length ; i<iLen ; i++ )
-    {
+     * filters, these will need to be reset*/
+    for ( var i=0, iLen=oSettings.aoPreSearchCols.length ; i<iLen ; i++ ){
         oSettings.aoPreSearchCols[i].sSearch = "";
     }
-      
     /* Redraw */
     oSettings.oApi._fnReDraw( oSettings );
 };
+
+jQuery.extend(jQuery.fn.dataTableExt.oSort, {
+    "title-asc": function(a, b){
+        return ((a < b) ? -1 : ((a > b) ? 1 : 0));
+    },
+ 
+    "title-desc": function(a, b) {
+        return ((a < b) ? 1 : ((a > b) ? -1 : 0));
+    }
+});
