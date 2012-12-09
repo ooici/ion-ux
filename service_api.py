513312e10a6ce173b8011f8148684d92762a8b99
Todd Boteler <tboteler@gmail.com>
2012-12-09 11:06:02 -0800

    Reenabled console logging.

--- service_api.py
+++ service_api.py
@@ -455,5 +455,5 @@ def service_gateway_agent_request(agent_id, operation_name, params={}):
             return resp['data']['GatewayResponse'][0]
 
 def pretty_console_log(label, content, data=None):
-    pass
-    # print '\n Service Gateway: ', '%s : %s' % (label, content), '\n\n'
+    # pass
+    print '\n Service Gateway: ', '%s : %s' % (label, content), '\n\n'
