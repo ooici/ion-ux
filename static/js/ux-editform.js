63df668f22b53b53e5f0042fb507cbb75f043246
Alex Clemesha <clemesha@ucsd.edu>
2013-02-13 17:28:58 -0800

    NestedFormModel now working, creating default 'Text' form elements.

--- static/js/ux-editform.js
+++ static/js/ux-editform.js
@@ -7,139 +7,61 @@ NOTES:
 
 
 BLACK_LIST: ['_id', '_rev', 'ts_created', 'ts_updated', 'lcstate', 'type_'];
+
+
+
+Form styling:
+  - removed top level data name (eg 'Resource')
+  - Replace '_' with ' '.
+  - Nested data: "(index+1)*margin_left" ...
+  - Sort order: alphabetically
 */
 
 
-var User = Backbone.Model.extend({
-    schema: {
-        title:      { type: 'Select', options: ['Mr', 'Mrs', 'Ms'] },
-        name:       'Text',
-        email:      { validators: ['required', 'email'] },
-        birthday:   'Date',
-        //password:   'Password',
-        //address:    { type: 'NestedModel', model: Address },
-        //notes:      { type: 'List', listType: 'Text' }
-    }
-});
+var NestedFormModel = Backbone.DeepModel.extend({
 
-//var user = new User({'title':'Mr', name:'Ed', email:'a@a.com', birdthday:'1/1/1900', password:'secret', notes:['Note 1', 'Note 2']});
-var user = new User({'title':'Mr', name:'Ed', email:'a@a.com', birdthday:'1/1/1900'});
-f = new Backbone.Form({ model: user }).render();
-//console.log(f.el);//new Backbone.Form({ model: user }).render());
+  initialize: function(attrs, options){
+    this.black_list = options.black_list || [];
+  },
 
+  schema: function(){
+    return this.make_schema();
+  },
 
-
-
-
-var DMS = Backbone.DeepModel.extend({
-    schema: {
-        'id':'Number',
-        'user.type':'Text',
-        'user.name.first':'Text',
-        'user.name.last':'Text',
-        'others.0.name': 'Text',
-        'others.1.name': 'Text'
-    }
-});
-TDM = new DMS({id: 123,
-    user: {type: 'Mgmt', name: {first: 'Sterling', last: 'Archer' }},
-    others: [ { name: 'Lana' }, { name: 'Cyrril' }]
-  });
-
-f2 = new Backbone.Form({ model: TDM }).render();
-console.log(f2.el);
-$("#dynamic-container").append(f2.el);
-
-
-
-
-
-
-
-
-
-
-var Resource = Backbone.DeepModel.extend({
-    schema: {
-      'message_controllable': 'Radio', 
-      'addl': 'Select', 
-      'description': 'Text',
-      'name': 'Text',
-      'alt_ids': 'Select',
-      'uuid': 'Text', 
-      'custom_attributes': 'Select', 
-      'hardware_version': 'Text', 
-      'monitorable': 'Radio', 
-      'controllable': 'Radio', 
-      'serial_number': 'Text', 
-      'firmware_version': 'Text',
-      'reference_urls': 'Select',
-      'last_calibration_datetime': 'Text', 
-      'contacts.individual_names_given': 'Text',
-      'contacts.city': 'Text',
-      'contacts.roles': {type: 'Select', options: ['primary', 'secondary', 'thirdary']}, 
-      'contacts.administrative_area': 'Text', 
-      'contacts.url': 'Text', 
-      'contacts.country': 'Text', 
-      //'contacts.variables': 'Select',
-      'contacts.organization_name': 'Text',
-      'contacts.type_': 'Text',
-      'contacts.postal_code': 'Text',
-      'contacts.street_address': 'Text'
-    }
-});
-
-
-var ResourceTopLevel = Backbone.Model.extend({
-  schema: {
-    message_controllable: 'Radio', 
-    addl: 'Select', 
-    description: 'Text',
-    name: 'Text',
-    alt_ids: 'Select',
-    uuid: 'Text', 
-    custom_attributes: 'Select', 
-    hardware_version: 'Text', 
-    monitorable: 'Radio', 
-    controllable: 'Radio', 
-    serial_number: 'Text', 
-    firmware_version: 'Text',
-    reference_urls: 'Select',
-    last_calibration_datetime: 'Text', 
+  make_schema: function(){
+    var paths = this.object_to_paths(this.attributes);
+    var tfield = function(){return "Text";}; //All 'Text' fields for now
+    var schema_full = _.object(paths, _.map(_.range(paths.length), tfield));
+    var schema = _.omit(schema_full, this.black_list); // remove black_listed
+    return schema
+  },
+  
+  object_to_paths: function(obj){
+    var result = [];
+    (function(o, r) {
+      r = r || '';
+      if (typeof o != 'object') {
+        return true;
+      }
+      for (var c in o) {
+        if (arguments.callee(o[c], r + "." + c)) {
+          result.push(r.substring(1) + "." + c);
+        }
+      }
+      return false;
+    })(obj);
+    return result;
   }
-})
 
-var ContactForResource = Backbone.Model.extend({
-  schema: {
-    individual_names_given: 'Text',
-    city: 'Text',
-    roles: {type: 'Select', options: ['primary', 'secondary', 'thirdary']}, 
-    administrative_area: 'Text', 
-    url: 'Text', 
-    country: 'Text', 
-    // variables: 'Select',
-    organization_name: 'Text',
-    type_: 'Text',
-    postal_code: 'Text',
-    street_address: 'Text'
-  }
 });
 
-var PhoneForContactForResource = Backbone.Model.extend({
-  schema: {
-    phone_number: 'Text', 
-    phone_type: {type: 'Select', options: ['home', 'office', 'cell']}, 
-    type_: 'Text',
-    sms: 'Radio'
-  }
-});
+
 
 
 IONUX.Models.EditableForm = Backbone.Model.extend({
   idAttribute: '_id',
 
-  modelmap: {"resource":Resource, "contacts":ContactForResource,
-            "phones":PhoneForContactForResource},
+  modelmap: {},
 
   initialize: function(attrs, options){
     this.black_list = options.black_list || [];
