2870f3abf47c8ea972162b04720714cea1ed344d
Todd Boteler <tboteler@gmail.com>
2013-09-12 16:04:01 -0700

    Check for UserInfo object in ServiceApi.update_resource.

--- service_api.py
+++ service_api.py
@@ -254,10 +254,7 @@ class ServiceApi(object):
         
         
         # reset session variable for IONUX.SESSION_MODEL on the client.
-        if resource_type == 'UserInfo':
-          for variable in resource_obj['variables']:
-            if variable['name'] == 'ui_theme_dark':
-              session['ui_theme_dark'] = variable['value']
+
         
         for k, v in resource_obj.iteritems():
             if isinstance(v, unicode) or isinstance(v, str):
@@ -281,8 +278,15 @@ class ServiceApi(object):
 
             if v == 'agent_config':
                 print v
-
-        req = service_gateway_post('resource_management', 'update_resource', params={'resource': resource_obj})
+        
+        if resource_type == 'UserInfo':
+          for variable in resource_obj['variables']:
+            if variable['name'] == 'ui_theme_dark':
+              session['ui_theme_dark'] = variable['value']
+          req = service_gateway_post('identity_management', 'update_user_info', params={'user_info': resource_obj})
+        else:
+          req = service_gateway_post('resource_management', 'update_resource', params={'resource': resource_obj})
+        
         reqs = [req]
 
         # handle associations
