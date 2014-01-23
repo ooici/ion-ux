783d6a9ee509e67c71196ef2b0ce88e84ce19a56
Charlton Galvarino <charlton@2creek.com>
2014-01-22 23:33:15 -0500

    OOIION-1678 Renamed /related_objects/ to /find_related_objects_has_resource/ and created two new services following the same pattern (find_related_objects_has_attachment and find_related_objects_has_role).  These services pull back resources where hasAttachment and hasRole are satisfied. This was done so that the Attachments and Roles filters could be applied in the face page's Long List.  Since I had to stick w/ 3 seperate web service calls, all three are fired, and when they are complete, the results are merged into data ready for the table.  And, voila, we now have attachments and roles appearing as hits.

--- static/js/ux-views-dashboard.js
+++ static/js/ux-views-dashboard.js
@@ -1351,7 +1351,7 @@ IONUX.Collections.ListResources = Backbone.Collection.extend({
     this.resource_id = options.resource_id;
   },
   url: function() {
-   return '/related_objects/'+this.resource_id+'/';
+   return '/related_objects_has_resource/'+this.resource_id+'/';
   },
   parse: function(resp) {
     var related_objects = [];
@@ -1361,6 +1361,35 @@ IONUX.Collections.ListResources = Backbone.Collection.extend({
   }
 });
 
+IONUX.Collections.ListRoles = Backbone.Collection.extend({
+  initialize: function(models, options){
+    this.resource_id = options.resource_id;
+  },
+  url: function() {
+   return '/related_objects_has_role/'+this.resource_id+'/';
+  },
+  parse: function(resp) {
+    var related_objects = [];
+    _.each(resp.data, function(obj){related_objects.push(obj)});
+    make_iso_timestamps(related_objects);
+    return related_objects;
+  }
+});
+
+IONUX.Collections.ListAttachments = Backbone.Collection.extend({
+  initialize: function(models, options){
+    this.resource_id = options.resource_id;
+  },
+  url: function() {
+   return '/related_objects_has_attachment/'+this.resource_id+'/';
+  },
+  parse: function(resp) {
+    var related_objects = [];
+    _.each(resp.data, function(obj){related_objects.push(obj)});
+    make_iso_timestamps(related_objects);
+    return related_objects;
+  }
+});
 
 /* 
 - - - - - - - - - - - - - - - - - 
